68022951d16d722fae0ee8f240a3b8561df280e2
huyifan <hu.yf@outlook.com>
2022-12-13 18:14:23 +0800

    fix

---
+++ 回溯问题/#46 全排列.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def __init__(self):
+        self.res = []
+        self.nums = None
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.nums = nums
+        self.dfs([])
+        return self.res
+
+    def dfs(self, track):
+        ## 结束条件
+        if len(track) == len(self.nums):
+            self.res.append(track[:])
+            return
+        for idx in range(len(self.nums)):
+            if self.nums[idx] in track:
+                continue
+            # 先进入
+            track.append(self.nums[idx])
+            self.dfs(track)
+            # 再推出
+            track.remove(self.nums[idx])
+
+
+s = Solution()
+print(s.permute([1,2,3]))
--- 图算法/并查集/#990 等式方程的可满足性.py
+++ 图算法/并查集/#990 等式方程的可满足性.py
@@ -1,48 +1,47 @@
+from typing import List
+
+
 class DSU(object):
     def __init__(self, N):
-        self.par = list(range(N))
-        self.size = [1] * N
-    def find(self, x):
-        while self.par[x] != x:
-            self.par[x] = self.par[self.par[x]]
-            x = self.par[x]
-        return x
-    def union(self, x, y):
-        xr = self.find(x)
-        yr = self.find(y)
-        if xr == yr:
-            return
-        if self.size[xr] > self.size[yr]:
-            self.par[yr] = xr
-            self.size[xr] += self.size[yr]
-        else:
-            self.par[xr] = yr
-            self.size[yr] += self.size[xr]
-    def connect(self, p, q):
-        return self.find(p) == self.find(q)
+        self.parents = list(range(N+1))
+        self.size = [1] * (N+1)
 
+    def find(self, x):
+        while self.parents[x] != x:
+            self.parents[x] = self.parents[self.parents[x]]
+            x = self.parents[x]
+        return x
+
+    def union(self, x, y):
+        xp, yp = self.find(x), self.find(y)
+        if xp == yp: return
+        if self.size[xp] > self.size[yp]:
+            self.parents[yp] = xp
+            self.size[xp] += self.size[yp]
+        else:
+            self.parents[xp] = yp
+            self.size[yp] += self.size[xp]
+
+    def is_connect(self, x, y):
+        return self.find(x) == self.find(y)
 
 class Solution:
-    def equationsPossible(self, equations) -> bool:
-        # 先统计一下有多少字母
-        if len(equations) < 1:
-            return True
-        chars = set(list("".join(equations).replace("=", "").replace("!", "")))
-        chars2id = {c: id for c, id in zip(chars, range(len(chars)))}
-        dsu = DSU(len(chars))
-        # 准备合并
-        for e in equations:
-            if '==' in e:
-                dsu.union(chars2id[e[0]], chars2id[e[-1]])
-        # 重新检查
-        for e in equations:
-            res = dsu.connect(chars2id[e[0]], chars2id[e[-1]])
-            if '==' in e:
-                if not res:
-                    return False
+    def equationsPossible(self, equations: List[str]) -> bool:
+        dsu = DSU(26)
+        # 先遍历== 再遍历!=
+
+        not_equal_list = []
+        for strs in equations:
+            if strs[1] == "=":
+                dsu.union(ord(strs[0])-ord("a"), ord(strs[-1])-ord("a"))
             else:
-                if res:
-                    return False
+                not_equal_list.append([ord(strs[0])-ord("a"), ord(strs[-1])-ord("a")])
+
+        # 逐一检验！=是否冲突
+        for a, b in not_equal_list:
+            ap, bp = dsu.find(a), dsu.find(b)
+            if ap == bp:
+                return False
         return True
 
 
